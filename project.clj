8591862b269483ba3d30f31756d872b2c37d0c04
Eric Meyers <etm@ericmeyers.com>
2014-02-23 09:47:24 -0500

    changed to MIT license

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject ninterpolate "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "The MIT  License"
+  :description "n-dimensional numeric table interpolator for Clojure"
+  :url "https://github.com/ericmeyers/ninterpolate"
+  :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
 
