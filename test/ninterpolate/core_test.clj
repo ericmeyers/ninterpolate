8242334d5f9d4172b11fcb1f1e424b494e6c7a54
Eric Meyers <etm@ericmeyers.com>
2014-02-23 10:58:06 -0500

    added more tests

--- test/ninterpolate/core_test.clj
+++ test/ninterpolate/core_test.clj
@@ -3,10 +3,11 @@
             [ninterpolate.core :refer :all]))
 
 
-(def table1d {1,2 3,4 5,6})
+(def table1d   {1,2 3,4 5,6})
+(def table1dus {5,6 1,2 3,4})
 
-(deftest simple-test
-  (testing "test a simple 1-d table"
-    (is (= 3.0 (linear_interpolate table1d 2.0)))
-  )
+(deftest linear-test
+    (is (= 3.0 (linear_interpolate table1d 2.0))   "sorted")
+    (is (= 3.0 (linear_interpolate table1dus 2.0)) "unsorted")
+    (is (= 0.0 (linear_interpolate table1dus -1.0)) "extrapolate")
 )
