7ecd539577e41730429cbac2d3df1c81d7c4665a
Eric Meyers <etm@ericmeyers.com>
2014-02-23 11:02:15 -0500

    more tests

--- test/ninterpolate/core_test.clj
+++ test/ninterpolate/core_test.clj
@@ -6,8 +6,21 @@
 (def table1d   {1,2 3,4 5,6})
 (def table1dus {5,6 1,2 3,4})
 
+(def table2d
+    {
+     1.0 {1,2 3,4 5,6 7,10.0}
+     2.0 {7,8 9,10.0, 11,12.0}
+    }
+)
+
 (deftest linear-test
+ (testing "univariant"
     (is (= 3.0 (linear_interpolate table1d 2.0))   "sorted")
     (is (= 3.0 (linear_interpolate table1dus 2.0)) "unsorted")
     (is (= 0.0 (linear_interpolate table1dus -1.0)) "extrapolate")
+ )
+ (testing "bivariant"
+    (is (= 2.0 (linear_interpolate table2d 1.0 1.0)) )
+    (is (= 9.0 (linear_interpolate table2d 1.5 7.0)) )
+ )
 )
