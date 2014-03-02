f8066a37b4e7a591a3b3a6c6cb7608ed8af70de3
Eric Meyers <etm@ericmeyers.com>
2014-03-02 12:51:55 -0500

    added ability to control degree of interpolation and whether or not extrapolation is allowed

--- src/ninterpolate/core.clj
+++ src/ninterpolate/core.clj
@@ -1,6 +1,9 @@
 (ns ninterpolate.core)
 ;(use 'clojure.tools.trace)
 
+
+
+
 (defn- bracket2 
     "returns a list containing 2 key value pairs that bracket the x value. the first 2 pairs or last 2 pairs are returned if x is 
      outside of the range of keys. note a key is always a number but a value can be a number or another list"
@@ -37,9 +40,10 @@
     )
 )
 
+(declare interpolate)
 
-(defn linear_interpolate
-     ([table xr]
+(defn- linear_interpolate
+     ([{extrap :extrap} table xr]
       {:pre [
              (isa? (class xr) Number)     
             ]
@@ -49,27 +53,28 @@
              slope (/ (- yh yl) (- xh xl))
              diff (- xr xl)
             ]
-            (+ yl (* diff slope)) 
+            (cond
+             (and (not extrap) (< xr xl)) yl
+             (and (not extrap) (> xr xh)) yh
+             :else (+ yl (* diff slope))
+            ) 
        )
      )
 
-     ([table xr1 & xrs]
+     ([opts table xr1 & xrs]
        (let [
              [[xl tablel] [xh tableh]] (bracket2 xr1 table)
-             yl (apply linear_interpolate tablel xrs)
-             yh (apply linear_interpolate tableh xrs)
+             yl (apply interpolate tablel xrs)
+             yh (apply interpolate tableh xrs)
             ]
-            (linear_interpolate {xl yl xh yh} xr1)
+            (linear_interpolate opts {xl yl xh yh} xr1)
       )
     )
 )
 
-(declare lagrange2_interpolate)
-;
-; this function can only be called if table has at least 3 items
-;
-(defn- lagrange2_interpolate_ok
-     ([table xr]
+
+(defn- lagrange2_interpolate
+     ([{extrap :extrap} table xr]
       {:pre [
              (isa? (class xr) Number)     
             ]
@@ -86,29 +91,55 @@
              xx2 (- xr xh1)
              xx3 (- xr xh2)
             ]
-            (+ (* xx3 (- (* q1 xx2) (* q2 xx1))) (* q3 xx1 xx2))
+            (cond
+              (and (not extrap) (< xr xl)) yl
+              (and (not extrap) (> xr xh2)) yh2
+              :else (+ (* xx3 (- (* q1 xx2) (* q2 xx1))) (* q3 xx1 xx2))
+           )
        )
      )
 
-     ([table xr1 & xrs]
+     ([opts table xr1 & xrs]
        (let [
              [[xl tablel] [xh1 tableh1] [xh2 tableh2]] (bracket3 xr1 table)
              yl (apply lagrange2_interpolate tablel xrs)
              yh1 (apply lagrange2_interpolate tableh1 xrs)
              yh2 (apply lagrange2_interpolate tableh2 xrs)
             ]
-            (lagrange2_interpolate {xl yl xh1 yh1 xh2 yh2} xr1)
+            (lagrange2_interpolate opts {xl yl xh1 yh1 xh2 yh2} xr1)
       )
     )
 )
 
 
+
+
+
+(defmulti interpolate (fn [table xr & xrs] (class xr)))
+
 ;
-; let's default to linear if table isn't long enough
+; no options supplied with the independent argument so just call linear interpolation without extrapolation
 ;
-(defn lagrange2_interpolate [table & args]
-    (cond
-     (>= (count table) 3) (apply lagrange2_interpolate_ok table args)
-     :else (apply linear_interpolate table args)
-    )
+(defmethod interpolate Number [table xr & xrs] 
+   (apply linear_interpolate {:extrap false} table xr xrs)
+)
+
+;
+; xr is a hash meaning some options were passed with the independent argument (interpolation order, allow extrapolation, etc)
+;
+(defmethod interpolate clojure.lang.PersistentArrayMap [table xr & xrs] 
+   {:pre [
+           (not (nil? (:value xr)))     
+         ]
+       }
+   (let [
+         xv (:value xr)
+         extrap (:extrap xr)
+         order (or (xr :order) 1)
+        ]
+        (cond
+         (= 1 order) (apply linear_interpolate {:extrap extrap} table xv xrs)
+         (= 2 order) (apply lagrange2_interpolate {:extrap extrap} table xv xrs)
+        )
+   )
 )
--- test/ninterpolate/core_test.clj
+++ test/ninterpolate/core_test.clj
@@ -3,8 +3,8 @@
             [ninterpolate.core :refer :all]))
 
 
-(def table1d   {1,2 3,4 5,6})
-(def table1dus {5,6 1,2 3,4})
+(def table1d   {1.0,2.0 3.0,4.0 7.0,6.0})
+(def table1dus {5.0,6.0 1.0,2.0 3.0,4.0})
 
 (def table2d
     {
@@ -13,15 +13,52 @@
     }
 )
 
+(def table3d
+  {
+   0.0
+    {
+      1.0 {1,2 3,4 5,6 7,10.0}
+      2.0 {7,8 9,10.0, 11,12.0}
+    }
+  
+   4.0
+    {
+      1.5 {11,2 13,4 15,6 7,10.0}
+      2.5 {17,8 19,10.0, 11,12.0}
+    }
+  }
+ 
+)
+
+
 (deftest linear-test
  (testing "univariant"
-    (is (= 3.0 (linear_interpolate table1d 2.0))   "sorted")
-    (is (= 3.0 (linear_interpolate table1dus 2.0)) "unsorted")
-    (is (= 0.0 (linear_interpolate table1dus -1.0)) "extrapolate")
+    (is (= 3.0 (interpolate table1d 2.0))   "sorted")
+    (is (= 3.0 (interpolate table1dus 2.0)) "unsorted")
+    (is (= 2.0 (interpolate table1dus -1.0)) "np extrapolate")
+    (is (= 0.0 (interpolate table1dus {:extrap true :value -1.0})) "extrapolate")
+
  )
  (testing "bivariant"
-    (is (= 2.0 (linear_interpolate table2d 1.0 1.0)) )
-    (is (= 9.0 (linear_interpolate table2d 1.5 7.0)) )
-    (is (= 10.5 (linear_interpolate table2d 1.5 8.0)) )
+    (is (= 2.0 (interpolate table2d 1.0 1.0)) "bivariant linear no extrap")
+    (is (= 9.0 (interpolate table2d 1.5 7.0)) "bivariant linear no extrap")
+    (is (= 9.5 (interpolate table2d 1.5 8.0)) "bivariant linear no extrap")
+    (is (= 10.5 (interpolate table2d 1.5 {:extrap true :value 8.0})) "bivariant linear extrap")
+    (is (= 15.0 (interpolate table2d {:extrap true :value 0.0} {:extrap true :value 8.0})) "bivariant linear extrap")
  )
+ (testing "trivariant"
+    (is (= 4.0 (interpolate table3d 0.0 1.0 3.0)) "3d")
+    (is (= 10.0 (interpolate table3d 4.0 1.5 7.0)) "3d")
+    (is (= 12.0 (interpolate table3d 4.0 2.5 7.0)) "3d")
+    (is (= 11.0 (interpolate table3d 4.0 2.0 7.0)) "3d")
+  )
+)
+
+(deftest lagrange-second-order-test
+ (testing "univariant"
+    (is (= 3.083333333333333 (interpolate table1d {:order 2 :value 2.0})))
+    (is (= 16.5 (interpolate table2d {:extrap true :value 0.0} {:order 2 :extrap true :value 8.0})) "bivariant linear extrap")
+ 
+ )
+
 )
